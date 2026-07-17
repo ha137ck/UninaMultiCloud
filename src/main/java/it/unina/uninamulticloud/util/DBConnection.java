8e01313b9ab6a2ea3a703c776a2b2694b79aa567
Luciano Addeo <lucianoaddeo7@gmail.com>
2026-07-17 16:40:20 +0200

    Creazione classe DBConnection.java per la gestione della connnessione al db

--- src/main/java/it/unina/uninamulticloud/util/DBConnection.java
+++ src/main/java/it/unina/uninamulticloud/util/DBConnection.java
@@ -8,4 +8,58 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class DBConnection {
+    private static DBConnection instance;
+    private Connection connection;
+
+    private static final String CONFIG_PATH = "/it/uninamulticloud/config/db.properties";
+
+    private DBConnection() {
+        try {
+            Properties props = new Properties();
+            try (InputStream input = DBConnection.class.getResourceAsStream(CONFIG_PATH)) {
+                if (input == null) {
+                    throw new RuntimeException("File db.properties non trovato in " + CONFIG_PATH);
+                }
+                props.load(input);
+            }
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            connection = DriverManager.getConnection(url, user, password);
+
+        } catch (IOException | SQLException e) {
+            throw new RuntimeException("Impossibile inizializzare la connessione al DB", e);
+        }
+    }
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                instance = new DBConnection();
+                return instance.connection;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Errore nel controllo dello stato della connessione", e);
+        }
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Errore nella chiusura della connessione", e);
+        }
+    }
 }
